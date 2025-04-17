8c731957e92496e4dcb721bb91677a89eb611a3a
Linux-Tech-Tips <disposablemail404.2@gmail.com>
2025-04-17 04:17:35 +0100

    Added program runtime features

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
-/* Main file, testing for now */
+/** 
+ * @file main.h
+ * @author Linux-Tech-Tips (Martin)
+ * @brief Main file, containing project runtime
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #include "matrix.h"
 #include "activation.h"
@@ -10,262 +15,188 @@
 #include "set.h"
 #include "util.h"
 
-int main(void) {
+#ifndef MAIN_NETWORK_FILENAME
+#define MAIN_NETWORK_FILENAME "active.net"
+#endif /* NETWORK_FILENAME */
 
+#ifndef MAIN_HEATMAP_ORIGIN_X
+#define MAIN_HEATMAP_ORIGIN_X -2
+#endif /* MAIN_HEATMAP_ORIGIN_X */
+#ifndef MAIN_HEATMAP_ORIGIN_Y
+#define MAIN_HEATMAP_ORIGIN_Y -1
+#endif /* MAIN_HEATMAP_ORIGIN_Y */
+
+#ifndef MAIN_HEATMAP_SIZE_X
+#define MAIN_HEATMAP_SIZE_X 4
+#endif /* MAIN_HEATMAP_SIZE_X */
+#ifndef MAIN_HEATMAP_SIZE_Y
+#define MAIN_HEATMAP_SIZE_Y 3
+#endif /* MAIN_HEATMAP_SIZE_Y */
+
+#ifndef MAIN_HEATMAP_STEP
+#define MAIN_HEATMAP_STEP 0.1f
+#endif /* MAIN_HEATMAP_STEP */
+
+void main_printHelp(char * programName);
+
+short main_loadNet(network_t * net, char const * networkFile);
+
+void main_train(char const * pointsFile, char const * configFile);
+
+void main_point(MATRIX_TYPE x, MATRIX_TYPE y);
+
+void main_heatmap(float originX, float originY, float sizeX, float sizeY, float step);
+
+int main(int argc, char ** argv) {
+
+    /* Initialising random number generator */
     srand(time(NULL));
 
-    matrix_t m1 = {0};
-    matrix_init(&m1, 2, 3);
-
-    matrix_t m2 = {0};
-    matrix_init(&m2, 3, 2);
-
-    matrix_set(&m1, 0, 0, 1);
-    matrix_set(&m1, 0, 1, 2);
-    matrix_set(&m1, 0, 2, 3);
-    matrix_set(&m1, 1, 0, 7);
-    matrix_set(&m1, 1, 1, 8);
-    matrix_set(&m1, 1, 2, 9);
-
-    matrix_set(&m2, 0, 0, 5);
-    matrix_set(&m2, 0, 1, 8);
-    matrix_set(&m2, 1, 0, 6);
-    matrix_set(&m2, 1, 1, -7);
-    matrix_set(&m2, 2, 0, 2);
-    matrix_set(&m2, 2, 1, 1);
-
-    /* Testing matrix multiplication */
-    matrix_t result = {0};
-    matrix_init(&result, 2, 2);
-
-    matrix_err_t mul = matrix_matmul(&m1, &m2, &result);
-
-    if(mul != MATRIX_OK) {
-	puts("Matrix Error!");
+    /* Checking command line arguments */
+    if(argc < 2) {
+	printf("Usage: '%s <command> <options>'\nTry '%s help'\n", argv[0], argv[0]);
 	return 1;
     }
 
-    matrix_print(&result);
+    /* Process command */
+    if(strcmp(argv[1], "help") == 0 || strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+	main_printHelp(argv[0]);
+	return 0;
 
-    activation_t a1 = activation_relu;
-    activation_t a2 = activation_logistic;
-
-    a1.f(&result);
-    matrix_print(&result);
-
-    a2.f(&result);
-    matrix_print(&result);
-
-    matrix_destroy(&m1);
-    matrix_destroy(&m2);
-
-    /* Testing network */
-
-    network_t testNet = {0};
-    size_t layers [] = {4, 1};
-    activation_t activations [] = {activation_logistic, activation_logistic};
-    network_init(&testNet, 3, 2, layers, activations);
-
-    matrix_t weights = {0};
-    matrix_init(&weights, 4, 3);
-    matrix_set(&weights, 0, 0, 0.8f); matrix_set(&weights, 0, 1, -1.1f); matrix_set(&weights, 0, 2, 1.0f);
-    matrix_set(&weights, 1, 0, -1.3f); matrix_set(&weights, 1, 1, -2.7f); matrix_set(&weights, 1, 2, 4.4f);
-    matrix_set(&weights, 2, 0, 0.2f); matrix_set(&weights, 2, 1, 2.1f); matrix_set(&weights, 2, 2, -1.1f);
-    matrix_set(&weights, 3, 0, 0.0f); matrix_set(&weights, 3, 1, 0.0f); matrix_set(&weights, 3, 2, 1.0f);
-    network_setWeights(&testNet, 0, &weights);
-    matrix_destroy(&weights);
-
-    matrix_init(&weights, 1, 4);
-    matrix_set(&weights, 0, 0, 1);
-    matrix_set(&weights, 0, 1, 1);
-    matrix_set(&weights, 0, 2, 1);
-    matrix_set(&weights, 0, 3, -2.75);
-    network_setWeights(&testNet, 1, &weights);
-    matrix_destroy(&weights);
-
-    network_setActivation(&testNet, 1, activation_relu);
-
-
-    matrix_t in = {0};
-    matrix_init(&in, 3, 1);
-    matrix_t out = {0};
-    matrix_init(&out, 1, 1);
-
-    char img [60][30] = {0};
-
-    for(size_t x = 0; x < 60; ++x) {
-	for(size_t y = 0; y < 30; ++y) {
-
-	    float inX = ((float)(x) - 20.0f) / 10.0f;
-	    float inY = ((float)(y) - 10.0f) / 10.0f;
-    	    matrix_set(&in, 0, 0, inX);
-    	    matrix_set(&in, 1, 0, inY);
-    	    matrix_set(&in, 2, 0, 1.0f);
-
-	    network_inference(&testNet, &in, &out);
-
-	    MATRIX_TYPE outNum;
-	    matrix_get(&out, 0, 0, &outNum);
-
-	    char outChar;
-	    if(outNum < 0.04)
-		outChar = '.';
-	    else if(outNum < 0.08)
-		outChar = ':';
-	    else if(outNum < 0.12)
-		outChar = '"';
-	    else
-		outChar = '#';
-	    img[x][y] = outChar;
+    } else if(strcmp(argv[1], "train") == 0) {
+	if(argc < 4) {
+	    printf("Error: not enough arguments for 'train' command\nTry '%s help'\n", argv[0]);
+	    return 1;
+	} else {
+	    main_train(argv[2], argv[3]);
 	}
-    }
-
-    /* Printing resulting map */
-    for(size_t y = 0; y < 30; ++y) {
-	for(size_t x = 0; x < 60; ++x) {
-	    printf("%c ", img[x][y]);
+	
+    } else if(strcmp(argv[1], "point") == 0) {
+	if(argc < 4) {
+	    printf("Error: not enough arguments for 'point' command\nTry '%s help'\n", argv[0]);
+	    return 1;
+	} else {
+	    MATRIX_TYPE x = 0, y = 0;
+	    sscanf(argv[2], MATRIX_TYPE_SCANF, &x);
+	    sscanf(argv[3], MATRIX_TYPE_SCANF, &y);
+	    main_point(x, y);
 	}
-	putchar('\n');
+
+    } else if(strcmp(argv[1], "heatmap") == 0) {
+	float originX = MAIN_HEATMAP_ORIGIN_X;
+	if(argc > 2)
+	    sscanf(argv[2], "%f", &originX);
+	float originY = MAIN_HEATMAP_ORIGIN_Y;
+	if(argc > 3)
+	    sscanf(argv[3], "%f", &originY);
+	float sizeX = MAIN_HEATMAP_SIZE_X;
+	if(argc > 4)
+	    sscanf(argv[4], "%f", &sizeX);
+	float sizeY = MAIN_HEATMAP_SIZE_Y;
+	if(argc > 5)
+	    sscanf(argv[5], "%f", &sizeY);
+	float step = MAIN_HEATMAP_STEP;
+	if(argc > 6)
+	    sscanf(argv[6], "%f", &step);
+	main_heatmap(originX, originY, sizeX, sizeY, step);
+
+    } else {
+	printf("Error: unrecognized command '%s'\nTry '%s help'\n", argv[1], argv[0]);
+	return 1;
     }
-
-    /* Testing inference with tracking */
-    MATRIX_TYPE n1 = 1.0f, n2 = 0.5f;
-    matrix_set(&in, 0, 0, n1);
-    matrix_set(&in, 1, 0, n2);
-    matrix_set(&in, 2, 0, 1.0f);
-
-    network_tracker_t testTracker = {0};
-    network_tracker_init(&testTracker, testNet.depth, layers);
-
-    network_inference_track(&testNet, &in, &out, &testTracker);
-
-    puts("Network internal nodes:");
-    puts("Layer 1:");
-    printf("(%.2f, %.2f)\n(%.2f, %.2f)\n(%.2f, %.2f)\n(%.2f, %.2f)\n", 
-	    testTracker.layerData[0][0][0], testTracker.layerData[0][0][1],
-	    testTracker.layerData[0][1][0], testTracker.layerData[0][1][1],
-	    testTracker.layerData[0][2][0], testTracker.layerData[0][2][1],
-	    testTracker.layerData[0][3][0], testTracker.layerData[0][3][1]);
-    puts("Layer 2:");
-    printf("(%.2f, %.2f)\n", testTracker.layerData[1][0][0], testTracker.layerData[1][0][1]);
-
-    network_tracker_destroy(&testTracker);
-
-    //matrix_destroy(&in);
-    //matrix_destroy(&out);
-
-    network_destroy(&testNet);
-
-    /* Testing training on dummy dataset */
-    network_t trainTest = {0};
-    size_t layers2 [2] = {5, 1};
-    activation_t activations2 [2] = { activation_logistic, activation_logistic };
-    network_init(&trainTest, 3, 2, layers2, activations2);
-    network_initWeights(&trainTest);
-
-    puts("Randomized weights:");
-    for(size_t idx = 0; idx < trainTest.depth; ++idx) {
-	matrix_print(trainTest.weights + idx);
-	putchar('\n');
-    }
-
-    set_t trainSet = {0};
-    set_init(&trainSet, 10, 3, 1);
-    MATRIX_TYPE inTrain [3] = {1.0f, 1.0f, 1.0f};
-    MATRIX_TYPE outTrain [1] = {1.0f};
-    set_setData(&trainSet, 0, inTrain, outTrain);
-    inTrain[0] = 0.6f; inTrain[1] = 0.6f; inTrain[2] = 1.0f;
-    outTrain[0] = 1.0f;
-    set_setData(&trainSet, 1, inTrain, outTrain);
-    inTrain[0] = 0.6f; inTrain[1] = 1.0f; inTrain[2] = 1.0f;
-    outTrain[0] = 1.0f;
-    set_setData(&trainSet, 2, inTrain, outTrain);
-    inTrain[0] = 1.0f; inTrain[1] = 0.6f; inTrain[2] = 1.0f;
-    outTrain[0] = 1.0f;
-    set_setData(&trainSet, 3, inTrain, outTrain);
-    inTrain[0] = 0.8f; inTrain[1] = 0.8f; inTrain[2] = 1.0f;
-    outTrain[0] = 1.0f;
-    set_setData(&trainSet, 4, inTrain, outTrain);
-
-    inTrain[0] = 0.4f; inTrain[1] = 1.2f; inTrain[2] = 1.0f;
-    outTrain[0] = 0.0f;
-    set_setData(&trainSet, 5, inTrain, outTrain);
-    inTrain[0] = 1.2f; inTrain[1] = 1.2f; inTrain[2] = 1.0f;
-    outTrain[0] = 0.0f;
-    set_setData(&trainSet, 6, inTrain, outTrain);
-    inTrain[0] = 1.2f; inTrain[1] = 0.4f; inTrain[2] = 1.0f;
-    outTrain[0] = 0.0f;
-    set_setData(&trainSet, 7, inTrain, outTrain);
-    inTrain[0] = 0.4f; inTrain[1] = 0.4f; inTrain[2] = 1.0f;
-    outTrain[0] = 0.0f;
-    set_setData(&trainSet, 8, inTrain, outTrain);
-    inTrain[0] = 0.0f; inTrain[1] = 0.0f; inTrain[2] = 1.0f;
-    outTrain[0] = 0.0f;
-    set_setData(&trainSet, 9, inTrain, outTrain);
-
-    set_train(&trainSet, &trainTest, layers2, 0.5f, 10000);
-
-    puts("Trained weights:");
-    for(size_t idx = 0; idx < trainTest.depth; ++idx) {
-	matrix_print(trainTest.weights + idx);
-	putchar('\n');
-    }
-
-    /* Testing weights on heatmap */
-    for(size_t x = 0; x < 60; ++x) {
-	for(size_t y = 0; y < 30; ++y) {
-
-	    float inX = ((float)(x) - 20.0f) / 10.0f;
-	    float inY = ((float)(y) - 10.0f) / 10.0f;
-    	    matrix_set(&in, 0, 0, inX);
-    	    matrix_set(&in, 1, 0, inY);
-    	    matrix_set(&in, 2, 0, 1.0f);
-
-	    network_inference(&trainTest, &in, &out);
-
-	    MATRIX_TYPE outNum;
-	    matrix_get(&out, 0, 0, &outNum);
-
-	    char outChar;
-	    if(outNum < 0.04)
-		outChar = '.';
-	    else if(outNum < 0.08)
-		outChar = ':';
-	    else if(outNum < 0.12)
-		outChar = '"';
-	    else
-		outChar = '#';
-	    img[x][y] = outChar;
-	}
-    }
-
-    /* Printing resulting map */
-    for(size_t y = 0; y < 30; ++y) {
-	for(size_t x = 0; x < 60; ++x) {
-	    printf("%c ", img[x][y]);
-	}
-	putchar('\n');
-    }
-
-    /* Verifying correctness on the set */
-    puts("Training data verification:");
-    for(size_t idx = 0; idx < trainSet.size; ++idx) {
-	matrix_copy((trainSet.in + idx), &in);
-	network_inference(&trainTest, &in, &out);
-	MATRIX_TYPE realOut, dOut;
-	matrix_get(&out, 0, 0, &realOut);
-	matrix_get((trainSet.out + idx), 0, 0, &dOut);
-	printf("%lu. Delta: %.5f\n", (idx + 1), (realOut - dOut));
-    }
-
-    /* Saving network */
-    util_saveNetwork(&trainTest, "last_net.dat");
-
-    set_destroy(&trainSet);
-    network_destroy(&trainTest);
-
-    matrix_destroy(&in);
-    matrix_destroy(&out);
 
     return 0;
+}
+
+void main_printHelp(char * programName) {
+    printf("Usage: '%s <command> <options>'\n", programName);
+    puts("available <command>s and their <options>:\n"
+	 "  - train <points> <config> ............ train neural network with given points and config files\n"
+	 "  - point <x> <y> ...................... run inference and provide an output value for a given point (x,y)\n"
+	 "  - heatmap [origin_x] [origin_y]\n"
+	 "            [size_x] [size_y] [step] ... run inference (optionally specify a custom area of size (size_x,size_y) from origin) and display heatmap\n"
+	 "  - --help | -h | help ................. display this help menu");
+}
+
+short main_loadNet(network_t * net, char const * networkFile) {
+    if(util_loadNetwork(net, networkFile) != UTIL_OK) {
+	printf("Error: Network could not be loaded\nCheck if file '%s' exists?\n", networkFile);
+	return 0;
+    }
+    return 1;
+}
+
+void main_train(char const * pointsFile, char const * configFile) {
+    /* Load config file */
+    util_config_t conf = {0};
+    if(util_loadConfig(&conf, configFile) != UTIL_OK) {
+	printf("Error: Config coould not be loaded\nCheck if file '%s' exists?\n", configFile);
+	return;
+    }
+
+    /* Load points file */
+    set_t set = {0};
+    if(util_loadPoints(&set, pointsFile) != UTIL_OK) {
+	printf("Error: Training points coould not be loaded\nCheck if file '%s' exists?\n", pointsFile);
+	return;
+    }
+
+    /* Initialize network */
+    network_t net = {0};
+    size_t layers [2] = { conf.hiddenSize, 1 };
+    activation_t activations [2] = { activation_get(conf.hiddenActivation), activation_get(conf.outputActivation) };
+    network_init(&net, 3, 2, layers, activations);
+    /* Set weights configs and initialize weights */
+    network_weightRandMin = conf.weightRandMin;
+    network_weightRandMax = conf.weightRandMax;
+    network_weightRandDiv = conf.weightRandDiv;
+    network_initWeights(&net);
+
+    /* Run training */
+    set_train(&set, &net, layers, conf.learningRate, conf.itCount);
+
+    /* Save network */
+    util_saveNetwork(&net, MAIN_NETWORK_FILENAME);
+
+    /* Dispose of any allocated/initialised resources */
+    set_destroy(&set);
+    network_destroy(&net);
+}
+
+void main_point(MATRIX_TYPE x, MATRIX_TYPE y) {
+    /* Load network */
+    network_t net = {0};
+    main_loadNet(&net, MAIN_NETWORK_FILENAME);
+
+    /* Set up and run inference */
+    matrix_t in = {0}, out = {0};
+    matrix_init(&in, 3, 1);
+    matrix_init(&out, 1, 1);
+    matrix_set(&in, 0, 0, x);
+    matrix_set(&in, 1, 0, y);
+    matrix_set(&in, 2, 0, 1.0f);
+    network_inference(&net, &in, &out);
+
+    /* Print result */
+    MATRIX_TYPE outVal = 0;
+    matrix_get(&out, 0, 0, &outVal);
+    printf("Inference result: (" MATRIX_TYPE_PRINTF ", " MATRIX_TYPE_PRINTF ") -> (" MATRIX_TYPE_PRINTF ")\n", x, y, outVal);
+
+    /* Dispose of any allocated resources */
+    matrix_destroy(&in);
+    matrix_destroy(&out);
+    network_destroy(&net);
+}
+
+void main_heatmap(float originX, float originY, float sizeX, float sizeY, float step) {
+    /* Load network */
+    network_t net = {0};
+    main_loadNet(&net, MAIN_NETWORK_FILENAME);
+
+    /* Generate heatmap */
+    char charset [7] = {'.', ',', '-', ';', '!', 'I', 'H'};
+    util_heatmap(&net, originX, originY, sizeX, sizeY, step, charset, 7);
+
+    /* Dispose of any allocated resources */
+    network_destroy(&net);
 }
